53ad281e852903f18acb3b837a26546478fc103a
mjanus <Milan.Janus@iais.fraunhofer.de>
2020-04-27 15:00:49 +0200

    second_script.py improvement

--- ReadMe second_script.txt
+++ ReadMe second_script.txt
@@ -1,18 +1,30 @@
 
 
-Use Case
---> Flags werden für Regelung der State Machine eingesetzt
---> State Machine startet die .log Datei zeilenweise auf Meldung '..server started..' zu durchsuchen
-    --> Meldung '..server started..'  wird mit Hilfe der Funktion 'get_server_start_line'  ausgewertet
-	--> Wenn Meldung '..server started..' gefunden ist
-		 --> Dann werden nächste vier Zeilen des Dokuments nach dem nach Meldung '..number of load programs..'   durchsucht
-		      --> Meldung '..server started..'  wird mit Hilfe der Funktion 'get_number_load_programs_line'  ausgewertet
-		      --> Falls  Meldung  '..number of load programs..'   gefunden wird
-			       --> Dann werden beide Meldungen zur Auswertung an Funktion 'create_log_message_entry' übergeben
-			            --> Hier sind  Fällen möglich
-						     --> Meldung '..server started..'  hat kein entsprechenes Gegen-Meldung '..number of load programs..'  
-							 --> Meldung '..server started..'  hat entsprechenes Gegen-Meldung '..number of load programs..'  UND Start hat planmässig stattgefunden
-							 --> Meldung '..server started..'  hat entsprechenes Gegen-Meldung '..number of load programs..'  UND Start hat NICHT planmässig stattgefunden							 
-		      --> Falls  Meldung  '..number of load programs..'  NICHT gefunden wird	
-			       --> Dann werden beide Meldungen zur Auswertung an Funktion 'create_log_message_entry' übergeben
+Was ist die Aufgabe des Scripts
+--> Wurde der Server gestartet und wurden die User-Programme geladen: Bilden beide Meldungen ein Paar?     
+--> Wurde der Server planmäßig um 03:00 gestartet?
+
+Ablauf
+--> Funktion "main" ist Startpunkt des Skripts und sie ruft die State Machine Funktion "find_enties" aus
+--> State Machine Funktion "find_entities" 
+     --> Funktion öffnet die aktuelle Logdatei 
+     --> Funktion	startet eine zeilenweise Suche nach der Meldung "..server started at.."
+     --> Machine State hat Status "wait" bis diese Meldung gefunden wird
+     --> Wenn diese Meldung gefunden wird, wechselt die State Machine in den aktiven Status 
+	      --> Jetzt startet die Funktion eine zeilenweise Suche nach der Meldung "..Number of programs stored.." 
+	      --> FALLS  Meldung  '..number of load programs..'   gefunden wird ODER FALLS  Meldung  '..number of load programs..'  NICHT gefunden wird:
+			  --> Die relevanten Daten beider Meldungen werden zur Auswertung an Funktion 'create_log_message_entry' übergeben 
+			  --> Der Unterschied zwischen zwei Fällen liegt im Inhalt der relevanten Daten für Meldung "..Number of programs stored.."
                       				   
+Use Case Fall 
+--> Cron-Daemon führt das Skript zur Auswertung der Log File aus
+--> Skript lädt Log File und wertet es aus
+--> Nach Auswertung des Skripts wird das Ergebnis in einer Ausgabe Log File ausgegeben 
+--> In der Ausgabe Log File sind folgende Fälle möglich:
+	 --> Server startete planmässig und Programme sind geladen 
+	 --> Server startete NICHT planmässig und Programme sind geladen 
+	 --> Server startete planmässig und Programme sind NICHT geladen 			
+	 --> Server startete NICHT planmässig und Programme sind NICHT geladen 		
+	 
+	 
+
--- second_script.py
+++ second_script.py
@@ -1,88 +1,46 @@
 import re
+import sys
 
-def get_number_loaded_programs_message(prog_load_number, message):    
-    if len(prog_load_number) == 0: 
-        return message + 'ERROR: missing number of load programs ' 
-    elif len(prog_load_number) < 6:
-        return message + 'WARNING: number of load programs is to low ' 
-    else:
-        return message + 'INFO: loaded ' + prog_load_number + ' programs'   
-    
-def create_log_message_entry(server_start_entry, prog_load_entry):          
-    if prog_load_entry == 'missing' and server_start_entry[1]  == '03:00' :        
-        message = 'ERROR: server started at: ' + server_start_entry[0] + ' an programs are not loaded'
-    elif prog_load_entry == 'missing' and server_start_entry[1]  != '03:00' :
-        message =  'ERROR: server started UNSCHEDULED at: ' + server_start_entry[0] + ' and programs are not loaded'     
-    else:         # 'prog load entry' match to 'server start entry' found AND server start time IS correct
-        message = get_number_loaded_programs_message(prog_load_entry[2]  ,'INFO: server started at: ' + server_start_entry[0] + ' and ')        
-    with open('output.log', 'a+') as output: 
-        output.write(message + "\n")            
+# Funktion zur Generierung des Messages und für die Ausgabe in Datei 'output.log'
+def create_log_message_entry(server_start_entry, prog_load_entry):      
+    with open('output.log', 'a+') as output:     
+        if prog_load_entry != 'missing' and server_start_entry[1] == '03:00':
+            output.write('INFO: server started at: ' + server_start_entry[0] + ';  INFO: loaded ' + prog_load_entry[1] + ' programs' + "\n")  
+        elif prog_load_entry != 'missing' and server_start_entry[1]  != '03:00' :    
+            output.write('ERROR: server started UNSCHEDULED at: ' + server_start_entry[0] + '; INFO: loaded ' + prog_load_entry[1] + ' programs' + "\n")  
+        elif prog_load_entry == 'missing' and server_start_entry[1] == '03:00' :
+            output.write('INFO: server started at: ' + server_start_entry[0] + ': ERROR programs are not loaded' + "\n")
+        else:
+            output.write('ERROR: server started UNSCHEDULED at: ' + server_start_entry[0] + ': ERROR programs are not loaded' + "\n")
 
-def get_date_hour_min(zeile):    
-    date_hour_min = re.match(r"\d{4}-\d{2}-\d{2} (\d{2}:\d{2})", zeile)
-    return [ date_hour_min.group(), date_hour_min.group(1) ]
-    
-def get_server_start_line(zeile):    
-    if re.search(r"server started at http://0.0.0.0:1999" , zeile)  is not None:
-        date_hour_min = get_date_hour_min(zeile) 
-        return [ date_hour_min[0],  date_hour_min[1] ]
-    else:
-        return None     
-        
-def get_number_load_programs_line(zeile):    
-    prog_load_number =  re.findall(r"\[(\d{1,})\]", zeile)
-    if len(prog_load_number) == 0:
-        prog_load_number = ''
-    else:
-        prog_load_number =  prog_load_number[0] 
-    if  len( re.findall(r"Number of programs stored in the database", zeile) ) > 0:  
-        date_hour_min = get_date_hour_min(zeile) 
-        return [ date_hour_min[0],  date_hour_min[1],  prog_load_number]
-    else:
-        return None            
-        
-def state_machine():
+# State Machine Funktion zur Suchen der Meldungen
+def find_enties():
     status_server_start = "wait"
-    status_prog_load = "wait"
     counter = 0
-    with open('04.log', 'r') as eingabe:             
+    with open('ausgabe.log', 'r') as eingabe:             
         for zeile in eingabe:
-            if  status_server_start != "run":             
-                server_start_line = get_server_start_line(zeile)   
-                # found .. 'server start ..' entry
-                if  server_start_line  is not None:    
-                    status_server_start = "run"
+            date = re.match(r"\d{4}-\d{2}-\d{2} (\d{2}:\d{2})", zeile)
+            if  status_server_start == "wait": 
+                if  re.search(r"server started at http://0.0.0.0:1999" , zeile)  is not None: 
+                    status_server_start =  [ date[0],  date[1] ]
                     continue
-            # search '.. programs stored ..' entry         
-            if  status_server_start == "run"  and status_prog_load == "wait": 
-                if counter < 4:
+            if  status_server_start != "wait": 
+                if counter < 2:
                     counter = counter + 1
-                    number_load_programs_line = get_number_load_programs_line(zeile)
-                    # found '.. programs stored ..' entry
-                    if  number_load_programs_line is not None: 
-                        if server_start_line[0] == number_load_programs_line[0]:
-                            create_log_message_entry(server_start_line, number_load_programs_line)
-                            status_server_start = "wait"
-                            status_prog_load = "wait"
-                            counter = 0
-                            continue
-                # entry '.. programs stored ..' NOT found        
+                    if  len( re.findall(r"Number of programs stored in the database", zeile) ) > 0 : 
+                        create_log_message_entry(status_server_start, [ date[0] , re.findall(r"\[(\d{1,})\]", zeile)[0]] )
+                        status_server_start = "wait"
+                        counter = 0
                 else:
-                    create_log_message_entry(server_start_line, "missing")
+                    create_log_message_entry(status_server_start, "missing")
                     status_server_start = "wait"
-                    status_prog_load = "wait"
-                    counter = 0                           
-                    continue        
+                    counter = 0                             
                     
 def main():
-    state_machine()
+    find_enties()
 
 if __name__ == "__main__":
-    main()
-
-
-        
-        
+    main()      
         
         
         
