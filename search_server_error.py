193060945b392b8d21e321d708849ba4051a1f5d
mjanus <Milan.Janus@iais.fraunhofer.de>
2020-05-06 16:58:40 +0200

    added Enum

--- search_server_error.py
+++ search_server_error.py
@@ -1,99 +1,105 @@
 import re
 import sys
+from enum import Enum
             
-def check_if_sheduled(start_time):
-    """ Evaluates if server start is  sheduled """
-    sheduled_starts = ['03:00' , '03:01' , '03:02' , '03:03']
+def check_if_scheduled(start_time):
+    """ Evaluates if server start is scheduled """
+    scheduled_starts = ['03:00' , '03:01' , '03:02' , '03:03']    
+    return start_time in scheduled_starts 
     
-    return start_time in sheduled_starts
+error_list = list()
+""" list for counting of errors """ 
 
-def evaluate_server_start_entry(start_date, start_time, number_of_programs):    
-    """ Evaluates the server start messages """
-    sheduled_start = 'server started at: ' 
-    un_sheduled_start = 'server started UNSHEDULED at: '
-    programs_occurs = '; with number of programs entry' 
-    programs_dont_occurs =  ': ERROR no number of programs entry'    
+def evaluate_server_start_entry(start_date, start_time, number_of_programs, outfile):
+    """ Evaluates the server start messages
+    :param: start_date: date of start 
+    :param: start_time: hour and min of start
+    :param: number_of_programs:   
+    :param: outfile: file for output of evaluation    
+    """    
+    scheduled_start = 'start : ' 
+    un_scheduled_start = 'ERROR: unscheduled start: '
+    programs_occurs = '; programs : ' 
+    programs_dont_occurs =  ' ERROR:  no programs \n' 
     
-    if check_if_sheduled(start_time):     
-        if number_of_programs == "None":
-            return sheduled_start + start_date + programs_dont_occurs
+    with open(outfile, 'a+') as file:       
+        if check_if_scheduled(start_time):     
+            if number_of_programs == None:
+                error_list.append(start_date)  
+                file.write(scheduled_start + start_date + programs_dont_occurs)  
+            else:                
+                file.write(scheduled_start + start_date +  programs_occurs + number_of_programs + '\n')                
         else:
-            return sheduled_start + start_date +  programs_occurs + number_of_programs               
-    else:
-        if number_of_programs == "None":
-            return un_sheduled_start + start_date + programs_dont_occurs      
-        else:
-            return un_sheduled_start + start_date +  programs_occurs + number_of_programs
-  
-def search_server_start_entry(log_file):
-    """ Searches the log file for messages "..server started at.." and "..Number of programs stored.."   """
-    status_machine  = "wait for server start"
-    server_start_date = ""
-    programs_counter = 0
-    server_start_list = list() 
+            if number_of_programs == None:
+                error_list.append(start_date)     
+                file.write(un_scheduled_start + start_date + programs_dont_occurs)      
+            else:
+                file.write(un_scheduled_start + start_date +  programs_occurs + number_of_programs + '\n') 
+                error_list.append(start_date)    
+                
+class MachineState(Enum):
+    """ status for state machine """    
+    wait_for_server = 1
+    wait_for_programs = 2
     
+def search_server_start_entry(log_file, outfile):
+    """ Searches the log file for messages "..server started at.." and "..Number of programs stored.."   
+    :param: log_file: file to be evaluate
+    :param: outfile: file for output of evaluation    
+    """
+    machine_state  = MachineState.wait_for_server
+    programs_counter = 0    
     with open(log_file, 'r') as file:             
         for row in file:
-            if  status_machine == "wait for server start" and re.search(r"server started at http://0.0.0.0:1999" , row)  is not None:
+            if  machine_state == MachineState.wait_for_server:
+                if re.search(r"server started at http://0.0.0.0:1999" , row)  is not None:
                     date_of_entry = re.match(r"\d{4}-\d{2}-\d{2} (\d{2}:\d{2})", row)
-                    status_machine = "wait for program number" 
+                    machine_state = MachineState.wait_for_programs
                     programs_counter = 0
                     continue
-            if  status_machine == "wait for program number":
+                else:
+                    continue
+            elif  machine_state == MachineState.wait_for_programs:
                 if programs_counter < 4:
                     programs_counter = programs_counter + 1
-                    start_date = date_of_entry[0]
-                    start_time = date_of_entry[1]
                     programs_entry = re.search(r"Number of programs stored in the database: \[(\d{1,})\]", row)                    
                     if  programs_entry is not None: 
-                        number_of_programs = programs_entry[1]
-                        entry = evaluate_server_start_entry(start_date, start_time, number_of_programs)
-                        server_start_list.append(entry)
-                        status_machine = "wait for server start"
+                        evaluate_server_start_entry(date_of_entry[0], date_of_entry[1], programs_entry[1], outfile)
+                        machine_state = MachineState.wait_for_server
+                        continue
+                    else:
+                        continue
                 else:
-                    entry = evaluate_server_start_entry(start_date, start_time,  "None")
-                    server_start_list.append(entry)
-                    status_machine = "wait for server start"
-                    
-    return server_start_list
+                    evaluate_server_start_entry(date_of_entry[0], date_of_entry[1],  None, outfile)
+                    machine_state = MachineState.wait_for_server                    
+            else:
+                raise Exception('machine_state is invalid. The value of machine_state was: {}'.format(machine_state))
 
-def evaluate_month(server_start_list, outfile):      
-    """ Evaluates whether all server starts are regular """
-    error_list = list()
-    status_error_server_start = "no server start error"
-    
-    for row in server_start_list:
-         if re.search(r"ERROR", row)  is not None:
-            status_error_server_start = "server start error occurs"   
-            start_date = re.search(r"\d{4}-\d{2}-\d{2} (\d{2}:\d{2})", row)[0]
-            error_list. append(start_date)                       
-            
-    if status_error_server_start == "server start error occurs":
-        server_start_list.append("errors occurs on:")        
-        for element in error_list:
-            server_start_list.append("-> " + element) 
-    else:
-           server_start_list.append('all server starts are regular' + "\n")  
-           
-    return server_start_list
-    
 def main():
-    month_to_check = ""
-    outfile = ""
+    infile = ""
+    outfile = ""    
     
-    if len(sys.argv) == 3:
-        month_to_check = sys.argv[1]
+    try:
+        infile = sys.argv[1]
+    except IndexError:
+        print("input file parameter missing")        
+        sys.exit()        
+        
+    try:
         outfile = sys.argv[2]
+    except IndexError:
+        print("output file parameter missing")            
+        sys.exit()
+        
+    server_start_list = search_server_start_entry(infile, outfile)
+    if len(error_list) == 0:
+        with open(outfile, 'a+') as output_file:
+            output_file.write('\n all server starts are regular' + "\n")  
     else:
-       month_to_check = input("please enter input file (month.log) to check: ")  
-       outfile = input("please enter output file name: ")
-       
-    server_start_list = search_server_start_entry(month_to_check)
-    output_list = evaluate_month(server_start_list, outfile)    
-    
-    with open(outfile, 'w') as outfile:
-        for element in output_list:
-            outfile.write(element + "\n")
+        with open(outfile, 'a+') as output_file:
+            output_file.write('\n server starts ERRORS on:' + "\n")    
+            for element in error_list:
+                output_file.write(element + "\n")      
             
 if __name__ == "__main__":
     main()
