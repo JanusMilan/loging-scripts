0a092ffaf0f7b92f85b7bfdaa490c08636b9ad2d
mjanus <Milan.Janus@iais.fraunhofer.de>
2020-05-08 14:55:25 +0200

    added program for searching cross-compiler errors and improved program which searches for daily errors

--- search_daily_errors.py
+++ search_daily_errors.py
@@ -2,53 +2,82 @@ import re
 import sys
 from enum import Enum
 
+
 class MachineState(Enum):
     """ Enum-Class serves for implementation of state machine status """
     wait_for_server_start = 1
-    wait_for_next_server_start = 2       
+    wait_for_next_server_start = 2
+    wait_for_error_rest = 3       
+    no_error_rest = 4     
 
-def search_error_entries(input_file, day_to_check, output_file):
-    """ search for error entries in desired file
- 
-    :param: 
-    :param: 
-    """   
-    status_server_start = MachineState.wait_for_server_start
-    output_file = open(output_file, 'a+')    
-    with open(input_file, 'r') as input_file:            
-        for element in input_file:                        
-            server_start = re.search(r"server started at http://0.0.0.0:1999" , element)
-            if  status_server_start == MachineState.wait_for_server_start:
-                date_of_entry = re.match(r"\d{4}-\d{2}-(\d{2}) \d{2}:\d{2}", element)
-                if  date_of_entry[1] ==  day_to_check:
+    
+def search_error_entries(input_file, output_file, day_to_check_errors):
+    """ search for error entries in desired file between two server starts
+    :param: input_file: .log file with data to  evaluate
+    :param: output_file: output file for the evalation data
+    :param: day_to_check_errors: day of server start from when the errors are searched until the next server start
+    """
+    to_ignore = "DbcKeyExceptionMapper"
+    machine_state = MachineState.wait_for_server_start
+    status_error_rest = MachineState.no_error_rest 
+    outfile = open(output_file, 'a+')
+    with open(input_file, 'r') as infile:
+        for row in infile: 
+            server_start = re.search(r"server started at http://0.0.0.0:1999" , row)
+            date_of_entry = re.match(r"\d{4}-\d{2}-(\d{2}) \d{2}:\d{2}", row)
+            if  machine_state == MachineState.wait_for_server_start:            
+                if  date_of_entry is not None and date_of_entry[1] ==  day_to_check_errors:
                         if  server_start  is not None:
-                            status_server_start = MachineState.wait_for_next_server_start
-                            output_file.write(element)
+                            machine_state = MachineState.wait_for_next_server_start
+                            outfile.write(row)
                             continue
                         else:
                            continue
                 else:
                    continue
-            elif  status_server_start == MachineState.wait_for_next_server_start:
+            elif  machine_state == MachineState.wait_for_next_server_start:
                 if server_start is None:
-                    if date_of_entry is not None and re.search(r"ERROR",  element) is not None:      
-                        output_file.write(element)
-                        continue
+                    if date_of_entry is not None: 
+                        if re.search(r" ERROR ",  row) is not None and  re.search(to_ignore,  row) is None:
+                            status_error_rest =  MachineState.wait_for_error_rest                             
+                            outfile.write(row)
+                            continue
+                        else:
+                            status_error_rest = MachineState.no_error_rest  
+                            continue                        
                     else:
-                        continue
-                else: # zweiten server start aufnehmen
-                    output_file.write(element)
+                        if  status_error_rest == MachineState.wait_for_error_rest:                   
+                            outfile.write(row)
+                            continue
+                        else: 
+                            continue
+                else: 
+                    outfile.write(row)
                     break
             else:
-                raise Exception('machine_state is invalid. The value of machine_state was: {}'.format(status_server_start))        
-    output_file.close()
+                raise Exception('machine_state is invalid. The value of machine_state was: {}'.format(machine_state))    
+    outfile.close()
+ 
 
 def main():
-    """ main function """  
-    input_file = "04.log"
-    output_file = "TEST.txt"
-    day_to_check = "01"    
-    search_error_entries(input_file , day_to_check, output_file)
-    
+    """ main function """
+    infile = ""
+    outfile = ""
+    day_to_check_errors = ""
+    try:
+        infile = sys.argv[1]
+    except IndexError:
+        sys.exit( "input file parameter missing")
+    try:
+        outfile = sys.argv[2]
+    except IndexError:
+        sys.exit("output file parameter missing")
+    try:
+        day_to_check_errors = sys.argv[3]
+    except IndexError:
+        sys.exit("day to check parameter missing") 
+
+    search_error_entries(infile, outfile, day_to_check_errors)
+ 
 if __name__ == "__main__":
     main()
