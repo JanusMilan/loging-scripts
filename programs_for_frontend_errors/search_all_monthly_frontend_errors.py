6c80a47463be8dbea2c71053ff9bc527490ed7f7
mjanus <Milan.Janus@iais.fraunhofer.de>
2020-05-28 15:37:37 +0200

    added testfiles

--- programs_for_frontend_errors/search_all_monthly_frontend_errors.py
+++ programs_for_frontend_errors/search_all_monthly_frontend_errors.py
@@ -1,46 +1,38 @@
 import re
 import sys
-from enum import Enum
 
+errors_to_ignore =  [
+        "jsToAppInterface >invalid webview type caused by: [object Object]" 
+]
 
-class MachineState(Enum):
-    """ Enum-Class serves for implementation of state machine status """
-    wait_for_error = 1
-    wait_for_rest_of_error = 2
 
 def search_client_logger_error_errors(infile, outfile):
     """ for searching for all monthly frontend errors 
     :param: infile: .log file with data to  evaluate 
     :param: outfile: output file for the evalation data
     """
-    machine_state = MachineState.wait_for_error 
-    output_file = open(outfile, 'a+')
+    output_file = open(outfile, 'w+')
     with open(infile, 'r') as file: 
-        for row in file:
-            client_logger_error = re.search(r" d.fhg.iais.roberta.util.ClientLogger - log entry: \[\[ERR \]\] (.*)", row)
-            not_rest_of_error = re.match(r"\d{4}-\d{2}-\d{2} (\d{2}:\d{2})", row)
-            if  machine_state == MachineState.wait_for_error:     
-                if client_logger_error is not None:
-                    output_file.write(row)
-                    machine_state = MachineState.wait_for_rest_of_error             
+        for line_number,line in enumerate(file):
+            client_logger_error = re.match(r".*(INFO d.fhg.iais.roberta.util.ClientLogger - log entry: \[\[ERR \]\] \[\[TIME\]\] \d{1,} msec:|INFO d.fhg.iais.roberta.util.ClientLogger - log entry: \[\[ERR \]\]) (.*)", line)
+            if client_logger_error is not None:
+                if client_logger_error[2] not in errors_to_ignore:
+                    output_file.write(str(line_number)+"::"+client_logger_error[2] +"\n")        
                     continue
                 else:
-                    continue
-            elif machine_state == MachineState.wait_for_rest_of_error:         
-                if not_rest_of_error is None:
-                    output_file.write(row)
-                    continue
-                else:
-                    machine_state = MachineState.wait_for_error
                     continue
             else:
-                raise Exception('machine_state is invalid. The value of machine_state was: {}'.format(machine_state))
+                continue
     output_file.close() 
 
 def main():
-    """ main function serves for process control """
+    """ main function serves for process control 
+    how to run program: python3 search_all_monthly_frontend_errors.py test_infile_frontend_error.txt front_end_error_file.txt 
+    """
     infile = ""
     outfile = ""
+    # infile = "test_infile_frontend_error.txt"    
+    # outfile = "front_end_error_file.txt"
     try:
         infile = sys.argv[1]
     except IndexError:
@@ -50,6 +42,7 @@ def main():
     except IndexError:
         sys.exit("output file parameter missing")
     search_client_logger_error_errors(infile, outfile)    
+
 
 if __name__ == "__main__":
     main()        
