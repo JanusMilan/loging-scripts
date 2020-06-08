5d5ba22fe25c5f9dc21a31216c42b7b3363afc64
mjanus <Milan.Janus@iais.fraunhofer.de>
2020-06-08 08:48:50 +0200

    extensive improvements

--- programs_for_frontend_entries/extract_and_analyze_frontend_errors/extract_and_analyze_frontend_errors.py
+++ programs_for_frontend_entries/extract_and_analyze_frontend_errors/extract_and_analyze_frontend_errors.py
@@ -2,19 +2,17 @@ import re
 import sys 
 from enum import Enum
 from known_frontend_errors import FrontendErrors
-
-
-known_frontend_errors = FrontendErrors.known_frontend_errors
+from copy import deepcopy
 
 
 class MachineState(Enum):
     """ Enum-Class serves for implementation of state machine status """
     search_error = 1
-    found_error = 2   
-    
+    found_error = 2      
+
 
 def prepare_string_for_regex(string):  
-    """ for prepare of the strings for the regex syntax
+    """ for prepare of the strings for the regex syntax for processes of filtering
         :param: string: string to prepare
     """
     string = string.replace("[", "\[") 
@@ -27,7 +25,7 @@ def prepare_string_for_regex(string):
     
 
 def remove_regex_escapes(string):  
-    """ for removing regex escapes out of the strings
+    """ for removing regex escapes out of the strings for the output
         :param: string: string to process
     """
     string = string.replace("\[", "[") 
@@ -38,90 +36,122 @@ def remove_regex_escapes(string):
     string = string.replace('\$', '$')
     string = string.replace('.*', '')
     return string        
+   
 
-def analyze_error(infile):  
-    """ for analyzing the monthly errors: Assign errors to error group and error type
-        :param: infile: file to analyze
-    """       
-    with open(infile, 'r') as file: 
+def compile_regex_dict(known_frontend_errors_copy):  
+    """ for constructing of dictionary with compiled regexes of known frontend errors as value
+   :param: known_frontend_errors_copy: deepcopy of dictionary "known_frontend_errors"
+    """   
+    line_pattern = r".*(\[\[ERR \]\] \[\[TIME\]\] .* msec:|\[\[ERR \]\]).*"
+    for error_group,group_value  in known_frontend_errors_copy.items():     
+        for error_subgroup, values in group_value.items():      
+            known_frontend_errors_copy[error_group][error_subgroup] = re.compile(line_pattern + error_group + error_subgroup)               
+    return known_frontend_errors_copy       
+ 
+ 
+def search_known_error(known_frontend_errors, client_logger_error_regex_dict, line):  
+    """ for analyzing at this time known monthly errors: 
+   - registers the line where the error type is occurred, if line contain known error
+   :param: known_frontend_errors: dictionary of the errors, which are already registered
+   :client_logger_error_regex_dict:  dictionary with compiled regexes of monthly frontend errors, which are already registered in dictionary  "known_frontend_errors"
+   :line: actual line of the input file to be processed
+    """   
+    machine_state = MachineState.search_error
+    for error_group,group_value  in known_frontend_errors.items():     
+        for error_subgroup, values in group_value.items():    
+            regex = client_logger_error_regex_dict[error_group][error_subgroup]
+            if regex.match(line[1]) is not None:     
+                known_frontend_errors[error_group][error_subgroup].append(int(line[0]))
+                machine_state = MachineState.found_error                       
+                return MachineState.found_error
+            else:
+                continue
+    if machine_state == MachineState.search_error:
+        return MachineState.search_error
+    else:
+        raise Exception('machine_state is invalid. The value of machine_state was: {}'.format(machine_state))
+
+
+def search_unknown_error(known_frontend_errors, client_logger_error_regex_dict, line):
+    """ for analyzing at this time unknown monthly errors: 
+    - extends the dictionary  "known_frontend_errors" with new error type, if line contain unknown error for the first time 
+    - registers the line where the new error type is occurred
+   :param: known_frontend_errors: dictionary with the errors, which are already registered
+   :client_logger_error_regex_dict:  dictionary of compiled regexes of monthly frontend errors, which are already registered in dictionary  "known_frontend_errors"
+   :line: actual line of the input file to be processed
+    """   
+    error_group = "NEW ERROR TYPE: "      
+    error_subgroup_regex = prepare_string_for_regex(line[1].rstrip()) 
+    if ": \[\[ERR \]\] \[\[TIME\]\]" in error_subgroup_regex:              
+        error_subgroup_regex = re.split("\d{0,} msec: ", error_subgroup_regex) 
+    elif ": \[\[ERR \]\]" in error_subgroup_regex: 
+           error_subgroup_regex = error_subgroup_regex.split(": \[\[ERR \]\] ")  
+    else:
+        return MachineState.search_error              
+    if known_frontend_errors[error_group].get(error_subgroup_regex[1]) is None:    
+        known_frontend_errors[error_group].update({error_subgroup_regex[1] : []})  
+        known_frontend_errors[error_group][error_subgroup_regex[1]].append(int(line[0]))
+    else:     
+        known_frontend_errors[error_group][error_subgroup_regex[1]].append(int(line[0]))   
+    return MachineState.found_error   
+
+
+def filter_error_lines(infile, known_frontend_errors):  
+    """ for analyzing the monthly frontend errors: 
+    - read all log lines from a infile
+    - regulates the filtering of the data by distributing the lines of file on two functions
+    - the known errors are processed by function "search_known_error"
+    - the unknown errors are processed by function "search_unknown_error"
+    :param: infile:  file which is generated by the program "extract_monthly_frontend_entries.py"
+    :param: known_frontend_errors: dictionary of the errors, which are already registered
+    """               
+    client_logger_error_regex_dict = compile_regex_dict(deepcopy(known_frontend_errors))             
+    with open(infile, 'r') as file:         
         for line in file:
             machine_state = MachineState.search_error
-            error_line = re.split('::',line)               
-            line_pattern = r".*(\[\[ERR \]\] \[\[TIME\]\] .* msec:|\[\[ERR \]\]).*"
-            for error_group,group_value  in known_frontend_errors.items():     
-                for error_subgroup in group_value:                      
-                    if re.match(line_pattern + error_group + error_subgroup, error_line[1]) is not None:   
-                        known_frontend_errors[error_group][error_subgroup].append(int(error_line[0]))   
-                        machine_state = MachineState.found_error                       
-                        break
-                    else:                                 
-                        machine_state = MachineState.search_error
-                        continue
-                if  machine_state == MachineState.found_error:
-                    break
-                if machine_state == MachineState.search_error:
-                    continue
-                else:
-                    raise Exception('machine_state is invalid. The value of machine_state was: {}'.format(machine_state))
-            if machine_state == MachineState.search_error:           
-                error_group = "NEW ERROR TYPE: "   
-                error_subgroup = error_line[1].rstrip()
-                error_subgroup_regex = prepare_string_for_regex(error_subgroup) 
-                if ": \[\[ERR \]\] \[\[TIME\]\]" in error_subgroup_regex: 
-                    error_subgroup_regex = re.split("\d{0,} msec: ", error_subgroup_regex)
-                else:    
-                    if ": \[\[ERR \]\]" in error_subgroup_regex: 
-                       error_subgroup_regex = error_subgroup_regex.split(": \[\[ERR \]\] ")        
-                    else:
-                        continue
-                if re.match(line_pattern, error_line[1]) is not None:
-                    if known_frontend_errors[error_group].get(error_subgroup_regex[1]) is None:                    
-                        known_frontend_errors[error_group].update({error_subgroup_regex[1] : []})  
-                        known_frontend_errors[error_group][error_subgroup_regex[1]].append(int(error_line[0]))
-                    else:
-                        known_frontend_errors[error_group][error_subgroup_regex[1]].append(int(error_line[0]))   
-                else:
-                    continue
-            elif machine_state == MachineState.found_error:
+            line = re.split('::',line)        
+            machine_state = search_known_error(known_frontend_errors, client_logger_error_regex_dict, line)
+            if machine_state == MachineState.found_error:      
                 continue
+            elif machine_state == MachineState.search_error:     
+                machine_state = search_unknown_error(known_frontend_errors, client_logger_error_regex_dict, line) 
+                if machine_state == MachineState.found_error:   
+                    client_logger_error_regex_dict = compile_regex_dict(deepcopy(known_frontend_errors)) 
+                elif  machine_state == MachineState.search_error:
+                    continue
+                else:
+                    raise Exception('machine_state is invalid. The value of machine_state was: {}'.format(machine_state))                      
             else:
                 raise Exception('machine_state is invalid. The value of machine_state was: {}'.format(machine_state))     
-
+                        
                     
-def write_output_file(outfile):    
-    """ for formating of output and for output of frontend errors into outputfile
+def write_output_file(outfile, known_frontend_errors):    
+    """ for formating and for creating of the output file
         :param: outfile: output file for the evalation data
+        :param: known_frontend_errors: dictionary of the errors, which are already registered
     """                 
-    with open(outfile, 'w+') as file: 
+    with open(outfile, 'w+') as output_file: 
         for error_group,value  in known_frontend_errors.items():
-            file.write(remove_regex_escapes(error_group) + '\n')            
+            output_file.write(remove_regex_escapes(error_group) + '\n')            
             for error_subgroup in value:                
                 number_of_errors = len(known_frontend_errors[error_group][error_subgroup])
-                if 0 < number_of_errors < 10:  
-                    file.write("          " + remove_regex_escapes(error_subgroup)  + str(known_frontend_errors[error_group][error_subgroup]) + '\n')                       
+                if 0 < number_of_errors < 10:   
+                    all_error_appearances = str(known_frontend_errors[error_group][error_subgroup])
+                    output_file.write('{:10s} {:s} {:s}\n'.format(" ", remove_regex_escapes(error_subgroup), all_error_appearances))                    
                 elif number_of_errors  >= 10:                                    
                     first_error_appearance = known_frontend_errors[error_group][error_subgroup][0]
-                    file.write("          "+remove_regex_escapes(error_subgroup) + ": [number of errors: "+str(number_of_errors)+", first error appearance:"+str(first_error_appearance)+']\n')                      
+                    output_file.write('{:10s} {:s} {:s} {:d} {:s} {:d}{:s}\n'.format(" ", remove_regex_escapes(error_subgroup), ": [number of errors:", number_of_errors, ", first error appearance:", first_error_appearance, "]"))                       
                 else:
-                    continue                    
-
+                    continue         
     
 def main():
-    """ main function serves for process control     
-    """
-    infile = ""    
-    outfile = ""
+    """ main function serves for process control    
+    """    
     try:
-        infile = sys.argv[1]
+        filter_error_lines(sys.argv[1],  FrontendErrors.known_frontend_errors) 
+        write_output_file(sys.argv[2], FrontendErrors.known_frontend_errors)
     except IndexError:
-        sys.exit( "input file parameter missing")
-    try:
-        outfile = sys.argv[2]
-    except IndexError:
-        sys.exit("output file parameter missing")
-    analyze_error(infile)
-    write_output_file(outfile)    
-    
+        sys.exit("usage: extract_monthly_frontend_entries <infile> <outfile>") 
 
 if __name__ == "__main__":
     main()        
--- programs_for_frontend_entries/extract_monthly_frontend_entries/extract_monthly_frontend_entries.py
+++ programs_for_frontend_entries/extract_monthly_frontend_entries/extract_monthly_frontend_entries.py
@@ -3,8 +3,8 @@ import sys
 from enum import Enum
 
 
-""" list with entries to ignore """
-entries_to_ignore =  [
+""" list with lines to ignore """
+lines_to_ignore =  [
         ".*jsToAppInterface >invalid webview type caused by: \[object Object\]",
         ".*init .* view",
         ".*init .* delete",
@@ -12,64 +12,35 @@ entries_to_ignore =  [
         ".*no IOS Webview.*"
 ]
 
-
-""" list with compiled pattern of entries to ignore """
-pattern_entries_to_ignore = []
-for element in entries_to_ignore:
-    pattern_entries_to_ignore.append(re.compile(element))
-    
-    
-class MachineState(Enum):
-    """ Enum-Class serves for implementation of state machine status """
-    search_entry_to_ignore = 1
-    found_entry_to_ignore = 2   
-    
-
-def search_client_logger_entries(infile, outfile):
-    """ for searching of all monthly frontend entries and noting their line numbers
-    :param: infile: .log file with data to  evaluate 
-    :param: outfile: output file for the evalation data
+def filter_log_lines(infile, outfile):
+    """ read all log lines from a file, write the lines, that are
+    - client lines and
+    - do not match one of the pattern defined in lines_to_ignore
+    :param: infile: file with all log lines
+    :param: outfile: file with the filtered data
     """
-    line_pattern = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).*(INFO d.fhg.iais.roberta.util.ClientLogger - log entry: (\[\[ERR \]\] \[\[TIME\]\] .* msec:|\[\[TIME\]\] .* msec:|\[\[ERR \]\]|\[\[INFO\]\])) (.*)"
-    client_logger_regex = re.compile(line_pattern)
-    output_file = open(outfile, 'w+')
-    with open(infile, 'r') as file:  
-        for line_number,line in enumerate(file):
-            line_pattern = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).*(INFO d.fhg.iais.roberta.util.ClientLogger - log entry: (\[\[ERR \]\] \[\[TIME\]\] .* msec:|\[\[TIME\]\] .* msec:|\[\[ERR \]\]|\[\[INFO\]\])) (.*)"
-            client_logger_line = client_logger_regex.match(line)
-            if client_logger_line is not None:
-                machine_state = MachineState.search_entry_to_ignore
-                for pattern in pattern_entries_to_ignore: 
-                    relevant_client_logger_line = pattern.match(line)
-                    if relevant_client_logger_line is None:     
-                        continue
-                    else:
-                        machine_state = MachineState.found_entry_to_ignore
-                        break
-                if machine_state == MachineState.found_entry_to_ignore: 
-                    continue
-                else:                    
-                    output_file.write(str(line_number + 1) + "::" + client_logger_line[1]  + ": " + client_logger_line[3]  + " " + client_logger_line[4] + "\n")               
-            else:
-                continue
-    output_file.close() 
-    
-    
+    client_log_pattern = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).*(INFO d.fhg.iais.roberta.util.ClientLogger - log entry: (\[\[ERR \]\] \[\[TIME\]\] .* msec:|\[\[TIME\]\] .* msec:|\[\[ERR \]\]|\[\[INFO\]\])) (.*)"
+    client_log_pattern_regex = re.compile(client_log_pattern)
+    lines_to_ignore_regex = []
+    for line_to_ignore in lines_to_ignore:
+        lines_to_ignore_regex.append(re.compile(line_to_ignore))
+
+    with open(outfile, 'w+') as output_file:
+        with open(infile, 'r') as input_file:  
+            for line_number,line in enumerate(input_file):
+                client_logger_line = client_log_pattern_regex.match(line)
+                if client_logger_line is not None:
+                    for regex in lines_to_ignore_regex: 
+                        if regex.match(line) is not None:     
+                            break
+                    else:                    
+                        output_file.write('{:08d}::{:s}: {:s} {:s}\n'.format(line_number + 1,client_logger_line[1],client_logger_line[3],client_logger_line[4]))
+        
 def main():
-    """ main function serves for process control 
-    """
-    infile = ""
-    outfile = ""
     try:
-        infile = sys.argv[1]
+        filter_log_lines(sys.argv[1], sys.argv[2])  
     except IndexError:
-        sys.exit( "input file parameter missing")
-    try:
-        outfile = sys.argv[2]
-    except IndexError:
-        sys.exit("output file parameter missing")
-    search_client_logger_entries(infile, outfile)    
-
+        sys.exit("usage: extract_monthly_frontend_entries <infile> <outfile>") 
 
 if __name__ == "__main__":
     main()        
